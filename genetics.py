efbf860538bd8923d2cca0b5b8bdb12dfea22fc4
Will Yelton <willhyelton@gmail.com>
2019-08-09 23:27:27 -0400

    added a framework for the genetics class

---
+++ genetics.py
@@ -0,0 +1,24 @@
+class Genetics:
+    def __init__(self):
+        pass
+
+    def createInitalPopulation(self):
+        pass
+
+    def breed(self, parent1, parent2):
+        pass
+
+    def breedAll(self, parent1, parent2):
+        pass
+
+    def gameCycle(self, model):
+        pass
+
+    def killWeak(self, population):
+        pass
+
+    def breedToFull(self, population):
+        pass
+
+    def mutate(self, population):
+        pass
